799187393c045e33519d7b161770caf0047a8524
YamaD <me@dyama.net>
2022-06-05 07:26:47 +0900

    initial commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,10 @@
+export default {
+  lang: "ja",
+  title: "電算20s",
+  description: "神戸高専電算部20sのホームページ",
+  themeConfig: {
+    nav: [
+      { text: "d3bu.net", link: "https://d3bu.net" },
+    ],
+  },
+}
